4bd55fe81f49b2a5d10b0b8ba4e76ea3efd7f263
Aleksandr <LamzinAV@gmail.com>
2022-08-07 19:35:02 +0300

    array 2

---
+++ ex010/Program.cs
@@ -0,0 +1,18 @@
+﻿int [] array = { 11, 211, 31, 4, 15, 4, 17, 18, 19};
+
+int n = array.Length;
+int find = 4;
+
+int  index = 0;
+
+while (index < n)
+{
+
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1;
+    index++;
+}
